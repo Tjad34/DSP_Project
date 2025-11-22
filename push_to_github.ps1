c9f54e5c0a95aba48f7a1de37655a1d541d56d55
tjad34 <pmaditya34@gmail.com>
2025-11-22 12:42:24 +0530

    Initial commit: Network Traffic Anomaly Detection project

--- push_to_github.ps1
+++
@@ -1,6 +0,0 @@
-git remote add origin https://github.com/Tjad34/DSP_Project.git
-git add .
-git commit -m "Initial commit: Network Traffic Anomaly Detection project with cleaned code"
-git branch -M main
-git push -u origin main
-
